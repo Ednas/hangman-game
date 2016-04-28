36c2dee889f259fd975891b1f7115f349b2a8073
ErnaNeal <Erna@MyCFLRealEstate.com>
2016-04-27 20:42:44 -0400

    working on JS to make the keystroke work and added an array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,7 +6,16 @@
 
 var fruitWords = ['banana', 'tomato', 'apple', 'orange', 'kiwi', 'strawberry', 'dragonfruit', 'blueberry', 'pineapple'];
 
+var currentWord = fruitWords[Math.floor(Math.random() * fruitWords.length)];
+   console.log(currentWord);
+
 document.onkeyup = function(event) 
 {
-	var userGuess = String.fromCharCode(event.keyCode).lowerCase();
+	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+	console.log(userGuess);
+
+	document.getElementById('guessed').appendHTML = userGuess;
+
 }
+
+
