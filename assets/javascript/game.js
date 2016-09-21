cc559a0256a499544ba39d5ac84051c1f839e113
Ednas <edna@edna.tech>
2016-09-21 13:16:56 -0400

    Making blanks for guesses and removing .pop function as it's not appropriate

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,13 +6,20 @@
 
 //Array of fruitwords
 var fruitWords = ['banana', 'tomato', 'apple', 'orange', 'kiwi', 'strawberry', 'dragonfruit', 'blueberry', 'pineapple'];
-
+var blanksAndGuesses = []; //Guesses and blanks will be in this array
+var blanks = 0; //holds blanks spaces for the word chosen
 //Selects a fruitword at random
 var currentWord = fruitWords[Math.floor(Math.random() * fruitWords.length)];
    console.log(currentWord);
 
 //array for users guessed letters
 var guessedLetter = [];
+//array for users correct guesses
+var correctLetter = [];
+
+function makeBlanks(){
+	blanks = currentWord.length;
+};
 
 
 //once you click a button, makes it lowercase and adds to the user guess element ID
@@ -33,14 +40,23 @@ console.log(currentLetters);
 //If the users guessed letter = a current letter, it will pop that letter off the array 
 //and once the user has emptied the array, they win. If they use up more than 20 letters, then
 //they lose.
+for (var i = 0; i < currentLetters.length; i++) {
+	currentLetters[i]
 
-for (var i = currentWord.length - 1; i >= 0; i--) {
-	currentWord[i]
-	var goal = userGuess.match(currentLetters);
-console.log(userGuess + "it worked");
-currentLetters.pop(userGuess);
-console.log(currentLetters);
+if (currentLetters == userGuess){
+	console.log(userGuess);
+}
+else {
+	console.log("Wrong guess");
+}
 };
+// for (var i = currentWord.length - 1; i >= 0; i--) {
+// 	currentWord[i]
+// 	var goal = userGuess.match(currentLetters);
+// console.log(userGuess + "it worked");
+// currentLetters.pop(userGuess);
+// console.log(currentLetters);
+// };
 
 
 if (userGuess == currentLetters){
